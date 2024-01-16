b95dca439e8a0c7c266cc2139fd28942a94cf34d
yahyacodes <yahya.codin@gmail.com>
2024-01-16 12:15:11 +0300

    Completed assignment

--- server/models.py
+++ server/models.py
@@ -11,4 +11,8 @@ db = SQLAlchemy(metadata=metadata)
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
